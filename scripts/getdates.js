29c885bab955720d5ccdbc12c006a4e3d41b3cbc
NonatoDEv <non24003@byui.edu>
2025-10-29 00:05:11 -0500

    get-date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,7 @@
 /* obtain the current year and put into the HTML*/ 
 const currentYearSpan = document.getElementById("currentYear");/*looking for ID*/ 
 const today = new Date()/*create a variable*/
-const year = today.getFullYear(); /* obtain the year*/ 
+const year = today.getFullYear(); /* obtain the year for the currently date*/ 
 const footer = "©" + year + " Powered by nonatoDev - All rights reserved ";/* mixing strings with variable and get a text*/
 currentYearSpan.innerHTML = footer;/*putting into the HTML */
 /* obtain the last date and put into the HTML*/
