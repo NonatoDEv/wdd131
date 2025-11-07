ef21a1c086e69713df979914697b81f000e3a987
NonatoDEv <non24003@byui.edu>
2025-11-07 00:16:45 -0500

    temples

--- scripts/temples.js
+++ scripts/temples.js
@@ -7,3 +7,9 @@ const lastModifiedP = document.getElementById("lastModified");
 const lastModifiedDate = document.lastModified;
 const lastModifiedText = "Last modification: " + lastModifiedDate;
 lastModifiedP.innerHTML = lastModifiedText;
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+amButton.addEventListener("_______", () => {
+	navigation._______.toggle("_______");
+	hamButton.classList.toggle("open");
+});
