628b3f9614e2dbb78bb3c061bef5ae511e66d203
NonatoDEv <non24003@byui.edu>
2025-11-15 23:04:34 -0500

    place

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,25 @@
+/*putting information from javascript*/
+const updateCountryData = (country) =>{
+    document.getElementById('area').textContent = country.area;
+    document.getElementById('population').textContent = country.population;
+    document.getElementById('capital').textContent = country.capital;
+    document.getElementById('language').textContent = country.language;
+    document.getElementById('currency').textContent = country.curency;
+    document.getElementById('time-zone').textContent = country.timezone;
+    document.getElementById('calling-code').textContent = country.callingcode;
+    document.getElementById('tld').textContent = country.tld;
+};
+updateCountryData({
+    area: ' 295.6 km²',
+    population: ' 89,456',
+    capital: ' Ilo City(San Geronimo de Ilo)',
+    language: ' Spanish',
+    curency: ' Soles(S/.)',
+    timezone: ' UTC-5',
+    callingcode: ' +51',
+    tld: ' .pe',
+});
+/* get date and last modification and putting into Html*/
 const currentYearSpan = document.getElementById("currentYear");
 const today = new Date()
 const year = today.getFullYear(); 
