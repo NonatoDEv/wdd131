4df701714843b8f006a9d00dbe3da6cebd2aee57
NonatoDEv <non24003@byui.edu>
2025-12-01 01:15:50 -0500

    form

---
+++ scripts/form.js
@@ -0,0 +1,52 @@
+document.querySelector('#currentYear').textContent =`© ${new Date().getFullYear() } Powered by nonatoDev - All rights reserved`;
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+function populateProductDropdown() {
+  const productSelect = document.getElementById('pName'); 
+    productSelect.innerHTML = "";
+
+    const placeholderOption = document.createElement('option');
+    placeholderOption.textContent = "Choose a product...";
+    placeholderOption.value = "";
+    placeholderOption.disabled = true; 
+    placeholderOption.selected = true;
+
+    productSelect.appendChild(placeholderOption);
+
+    products.forEach(product => {
+        const optionElement = document.createElement('option');
+        optionElement.textContent = product.name;
+        optionElement.value = product.id;
+        productSelect.appendChild(optionElement);
+
+    });
+}
+populateProductDropdown(products);
