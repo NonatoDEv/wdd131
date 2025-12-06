a56e4fb4c11df5d2f3c66de01e3791786230fe08
NonatoDEv <non24003@byui.edu>
2025-12-06 15:16:40 -0500

    site-project

--- project/scripts/data.js
+++ project/scripts/data.js
@@ -21,7 +21,7 @@ export const allProducts = [
         src: "images/romper-spiderman.webp",
         price: 35.00,
         category: "ropa",
-        alt: "Cambiador plegable con compartimentos."
+        alt: "Romper con diseño de Spiderman, ideal para fiestas de disfraces."
     },
     {
         id: 4,
@@ -39,4 +39,44 @@ export const allProducts = [
         category: "ropa",
         alt: "Ajuares básicos para recién nacidos, incluye body, gorro y manoplas."
     },
+    {
+        id: 6,
+        name: "Set jhonsom x 5",
+        src: "images/jhonsom-setx5.webp",
+        price: 65.00,
+        category: "cuidado",
+        alt: "Set de cuidado personal para bebés, incluye shampoo, jabón líquido, crema para pañal y loción hidratante."
+    },
+    {
+        id: 7,
+        name: "Dosificador de papilla",
+        src: "images/dosificador-papilla.webp",
+        price: 23.00,
+        category: "accesorios",
+        alt: "Cuchara Dosificadora para alimentar las primeras papillas del bebe."
+    },
+    {
+        id: 8,
+        name: "Romper Spiderman",
+        src: "images/romper-spiderman.webp",
+        price: 35.00,
+        category: "ropa",
+        alt: "Romper con diseño de Spiderman, ideal para fiestas de disfraces."
+    },
+    {
+        id: 9,
+        name: "Mordedor de agua",
+        src: "images/mordedor.webp",
+        price: 15.00,
+        category: "accesorios",
+        alt: "Mordedor para pacificar las encias del bebe."
+    },
+    {
+        id: 10,
+        name: "Ajuares Angelitos",
+        src: "images/ajuares-basicos-angelitos.webp",
+        price: 12.00,
+        category: "ropa",
+        alt: "Ajuares básicos para recién nacidos, incluye body, gorro y manoplas."
+    }
 ];
